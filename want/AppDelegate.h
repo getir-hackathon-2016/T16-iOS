0068e753095c8e5cf8710be3add5dd3d50f96f43
Ömer Karışman <omerkarisman@gmail.com>
2016-02-19 23:16:02 +0200

    Initial Commit

---
+++ want/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  want
+//
+//  Created by Omer Karisman on 19/02/16.
+//  Copyright © 2016 Omer Karisman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ want/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  want
+//
+//  Created by Omer Karisman on 19/02/16.
+//  Copyright © 2016 Omer Karisman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
