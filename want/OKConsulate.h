5b1f7d57d315065a2fe70d273f5cba8f06a86312
Ömer Karışman <omerkarisman@gmail.com>
2016-02-20 00:39:47 +0200

    Updated deployment target to iOS 8.2. Introduced OKConsulate

---
+++ want/OKConsulate.h
@@ -0,0 +1,17 @@
+//
+//  OKConsulate.h
+//  want
+//
+//  Created by Omer Karisman on 20/02/16.
+//  Copyright © 2016 Omer Karisman. All rights reserved.
+//
+
+//  OKConsulate is developed to do all the talking with
+//  backend. It contains methods that might be used over
+//  and over again.
+
+#import <UIKit/UIKit.h>
+
+@interface OKConsulate : UIViewController
+
+@end
